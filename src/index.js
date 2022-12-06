94c212d63d4f7db19ceda2036986be6d78450206
RoyZiO <nziokinroy@gmail.com>
2022-12-06 12:34:36 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -10,13 +10,15 @@ inputForm.addEventListener('submit', (event) => {
 
     //event.target.children[1].value;
 
-    const input = document.querySelector('input#searchByID');
+    const input = document.querySelector('input#searchByID');   //This code enables us to access and input value directly.
 
 
-    fetch(`http://localhost:3000/movies/${input.value}`)
+    fetch(`http://localhost:3000/movies/${input.value}`)      //Here, we fetch data based on user input
     .then(response => response.json())
 
     .then(data => {
+
+        //Here, we display the fecthed data on the webpage 
 
         const title = document.querySelector('section#movieDetails h4');
 
